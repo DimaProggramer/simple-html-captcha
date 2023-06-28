08b5cc39ac1ca678d727b03f0965663f3065632d
DimaProggramer <135269406+DimaProggramer@users.noreply.github.com>
2023-06-28 23:21:45 +0300

    fix server.js
    im make a small bug with dirname

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const path = require('path');                                                                               const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname, '')));                                                     // Обработка запроса на корневой путь
-app.get('/', (req, res) => {                            res.sendFile(path.join(__dirname, 'doom.html'));    });                                   app.listen(port, () => {
+app.use(express.static(path.join(__dirname, '')));                                                 
+app.get('/', (req, res) => {                            res.sendFile(path.join(__dirname, 'index.html'));    });                                   app.listen(port, () => {
   console.log(`runned at ${port} port`);
 });
